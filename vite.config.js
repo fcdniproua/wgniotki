ddac63c450268102eaa9fb86cbbb1605774ff222
Alexandr Yemelianov <fcdnipro268@gmail.com>
2025-05-03 12:25:00 +0200

    Update slider_tag column and fix route HTTP methods
    Modified the `slider_tag` column in the `photos` table to be nullable, ensuring flexibility in data constraints. Updated HTTP methods for destroy routes to use `DELETE` instead of `POST` for better RESTful compliance. Adjusted validation rules for `slider_tag` in `PhotoController` to allow null values.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/build/assets/',
+    base: 'https://usuwanie-wgniecen.pro/build/assets/',
     build: {
         manifest: 'manifest.json',
         outDir: 'public/build',
