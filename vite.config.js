5fc61ca7ba96fe1b2c487a5a7f696ac7a4b2b766
Alexandr Yemelianov <fcdnipro268@gmail.com>
2025-05-03 12:18:01 +0200

    Update slider_tag column and fix route HTTP methods
    Modified the `slider_tag` column in the `photos` table to be nullable, ensuring flexibility in data constraints. Updated HTTP methods for destroy routes to use `DELETE` instead of `POST` for better RESTful compliance. Adjusted validation rules for `slider_tag` in `PhotoController` to allow null values.

--- vite.config.js
+++ vite.config.js
@@ -1,52 +1,76 @@
-import laravel from "laravel-vite-plugin";
-import vue from "@vitejs/plugin-vue";
-import {defineConfig} from "vite";
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/build/', // базовий префікс для ресурсів у продакшн-збірці
-
     build: {
         manifest: true,
         outDir: 'public/build',
         rollupOptions: {
-            input: 'resources/js/app.js'
-        }
-    },
-
-    server: {
-        host: '0.0.0.0',  // Обов'язково для Docker
-        port: 5173,       // Порт всередині контейнера
-        strictPort: true, // Не шукати вільні порти
-        cors: {
-            origin: ['http://192.168.233.172:8080','http://127.0.0.1:8080'], // або '*', але не бажано для продакшну
-            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            input: 'resources/js/app.js',
         },
-        hmr: {
-            // host: '127.0.0.1',
-            host: '192.168.233.172',
-            clientPort: 5173  // Порт для Hot Module Replacement
-        }
+        emptyOutDir: true,
     },
     plugins: [
         laravel({
-            input: ['resources/js/app.js'],
+            input: ['resources/js/app.js', 'resources/css/app.css'], // додай CSS
             refresh: true,
         }),
         vue(),
     ],
-    resolve: {
-        alias: {
-            '@': '/resources/js',
-        },
-    },
-    css: {
-        postcss: {
-            plugins: [
-                tailwindcss,
-                autoprefixer,
-            ],
-        },
-    },
-});
+})
+
+
+
+// import laravel from "laravel-vite-plugin";
+// import vue from "@vitejs/plugin-vue";
+// import {defineConfig} from "vite";
+// import tailwindcss from 'tailwindcss';
+// import autoprefixer from 'autoprefixer';
+//
+// export default defineConfig({
+//     base: '/build/', // базовий префікс для ресурсів у продакшн-збірці
+//
+//     build: {
+//         manifest: true,
+//         outDir: 'public/build',
+//         rollupOptions: {
+//             input: 'resources/js/app.js'
+//         }
+//     },
+//
+//     server: {
+//         host: '0.0.0.0',  // Обов'язково для Docker
+//         port: 5173,       // Порт всередині контейнера
+//         strictPort: true, // Не шукати вільні порти
+//         cors: {
+//             origin: ['http://192.168.233.172:8080','http://127.0.0.1:8080'], // або '*', але не бажано для продакшну
+//             methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//         },
+//         hmr: {
+//             // host: '127.0.0.1',
+//             host: '192.168.233.172',
+//             clientPort: 5173  // Порт для Hot Module Replacement
+//         }
+//     },
+//     plugins: [
+//         laravel({
+//             input: ['resources/js/app.js'],
+//             refresh: true,
+//         }),
+//         vue(),
+//     ],
+//     resolve: {
+//         alias: {
+//             '@': '/resources/js',
+//         },
+//     },
+//     css: {
+//         postcss: {
+//             plugins: [
+//                 tailwindcss,
+//                 autoprefixer,
+//             ],
+//         },
+//     },
+// });
