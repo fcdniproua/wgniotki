a08c9332b26ee39df1994b7acc647e15fd9007d3
Alexandr Yemelianov <fcdnipro268@gmail.com>
2025-05-05 20:35:31 +0200

    Update UI styles and improve accessibility across pages
    Changed design elements including background colors and font colors for consistency using #DAA520 and #F5F1EB. Enhanced readability and user experience with text updates, placeholder changes, and added bold styling. Improved footer and layout elements by refining social links and introducing a WhatsApp link.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
-<html>
+<html lang="pl">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <base href="/">
     @vite(['resources/js/app.js'])
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+    <title>Usuwanie wgnieceń bez lakierowania | Profesjonalna naprawa karoserii</title>
+
+    <meta name="description" content="Specjalizujemy się w bezinwazyjnym usuwaniu wgnieceń karoserii metodą PDR. Szybko, skutecznie i bez potrzeby lakierowania. Umów się już dziś!">
+    <meta name="robots" content="index, follow">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:title" content="Usuwanie wgnieceń bez lakierowania">
+    <meta property="og:description" content="Profesjonalna naprawa karoserii bez potrzeby lakierowania. Sprawdź nasze usługi!">
+    <meta property="og:url" content="https://usuwanie-wgniecen.pro/">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="https://usuwanie-wgniecen.pro/assets/img/logo_v4.jpg">
+    <link rel="canonical" href="https://usuwanie-wgniecen.pro/">
+
 </head>
 <body>
 <div id="app"></div>
